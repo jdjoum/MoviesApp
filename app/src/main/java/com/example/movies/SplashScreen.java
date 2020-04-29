9ff095a8b79e657967d7b4485fdc6b2d8c6808f3
jdjoum <jdjoum@hotmail.com>
2020-04-29 16:14:55 -0400

    Splash Screen

---
+++ app/src/main/java/com/example/movies/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.example.movies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    ImageView ivSplash;
+    Animation fromtop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        ivSplash = (ImageView) findViewById(R.id.ivSplash);
+        fromtop = AnimationUtils.loadAnimation(this,R.anim.fromtop);
+        ivSplash.setAnimation(fromtop);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+
+        },3000);
+
+    }
+}
